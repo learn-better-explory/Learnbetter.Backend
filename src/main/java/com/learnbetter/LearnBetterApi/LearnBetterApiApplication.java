c9a8bd65a7f4c90e2cc5557f5125f0fc336e12d2
trolca <szgiemza@gmail.com>
2024-11-16 21:09:02 +0100

    Initial commit

---
+++ src/main/java/com/learnbetter/LearnBetterApi/LearnBetterApiApplication.java
@@ -0,0 +1,13 @@
+package com.learnbetter.LearnBetterApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnBetterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnBetterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnbetter/LearnBetterApi/LearnBetterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnbetter.LearnBetterApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnBetterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
